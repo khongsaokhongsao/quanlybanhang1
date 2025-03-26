8582ad45af5fe836676bf3dd8c95eec6844b0e50
Vu Truc Lam <vutruclam1202@gmail.com>
2025-03-26 23:25:33 +0700

    danh sách sản phẩm, thêm sản phẩm chưa lưu sql

---
+++ Views/ProductFormView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyBanHang.Views
+{
+    /// <summary>
+    /// Interaction logic for ProductFormView.xaml
+    /// </summary>
+    public partial class ProductFormView : UserControl
+    {
+        public ProductFormView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
