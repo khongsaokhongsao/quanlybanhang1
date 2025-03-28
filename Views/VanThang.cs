ced07e03f2cd9b82faae0b20e9005513f03dbb5a
nvt21092005@gmail.com <nvt21092005@gmail.com>
2025-03-28 14:54:54 +0700

    thang cap nhat

---
+++ Views/VanThang.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHang.Views
+{
+    internal class VanThang
+    {
+    }
+}
