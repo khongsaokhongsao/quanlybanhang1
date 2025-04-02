45b19adaafc6127daaaf071562fb749651f08e18
vietn0021 <vietn0021@gmail.com>
2025-04-02 21:12:56 +0700

    Quy

---
+++ Repositories/QuyRepository.cs
@@ -0,0 +1,53 @@
+﻿using QuanLyBanHang.DataAccess;
+using QuanLyBanHang.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHang.Repositories
+{
+    public class QuyRepository
+    {
+        public List<Quy> GetAllQuy()
+        {
+            try
+            {
+                var quy = new List<Quy>();
+
+                using (var connection = DatabaseHelper.GetConnection())
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Products";
+                    using (var command = new SQLiteCommand(query, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var quyItem = new Quy
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1),
+                                    Type = reader.GetString(2),
+                                    Balance=reader.GetInt32(3),
+                                    TotalTransaction = reader.GetDouble(4),
+                                    Status = reader.GetString(5)
+                                };
+                                //Quy.Add(quy);
+                            }
+                        }
+                    }
+                }
+
+                return quy;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Lỗi khi lấy danh sách quỹ từ cơ sở dữ liệu: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ ViewModels/Quy/QuyList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using QuanLyBanHang.Models;
+using QuanLyBanHang.Repositories;
+using QuanLyBanHang.Helpers;
+using QuanLyBanHang.Views;
+using System.ComponentModel;
+
+
+
+namespace QuanLyBanHang.ViewModels
+{
+    public class QuyViewModel : INotifyPropertyChanged
+    {
+        private object _currentView;
+        private Quy _selectedQuy;
+        private readonly QuyRepository _quyRepository;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        public ObservableCollection<Quy> Quys { get; set; }
+        public Quy SelectedQuy
+        {
+            get => _selectedQuy;
+            set
+            {
+                _selectedQuy = value;
+                OnPropertyChanged(nameof(SelectedQuy));
+            }
+        }
+
+        // Command để thêm, sửa, xóa quỹ
+        public ICommand ShowQuyList { get; set; }
+        //public ICommand AddQuyCommand { get; set; }
+        //public ICommand EditQuyCommand { get; set; }
+        //public ICommand DeleteQuyCommand { get; set; }
+        public ICommand SaveQuyCommand { get; set; }
+
+        public QuyViewModel()
+        {
+            _quyRepository = new QuyRepository();
+            Quys = new ObservableCollection<Quy>(_quyRepository.GetAllQuy());
+
+            // Khởi tạo các command
+            ShowQuyList = new RelayCommand(ShowQuyListView);
+            //AddQuyCommand = new RelayCommand(AddQuy);
+            SaveQuyCommand = new RelayCommand(SaveQuy);
+
+            // Mặc định hiển thị danh sách quỹ
+            ShowQuyListView(null);
+        }
+
+        public void ShowQuyListView(object obj)
+        {
+            LoadQuys();
+            var quyListView = new QuyView
+            {
+                DataContext = this
+            };
+            CurrentView = quyListView;
+        }
+
+        //public void AddQuy(object obj)
+        //{
+        //    SelectedQuy = new Quy();
+        //    var quyFormView = new QuyFormView
+        //    {
+        //        DataContext = this
+        //    };
+        //    CurrentView = quyFormView;
+        //}
+
+        
+
+        private void SaveQuy(object obj)
+        {
+            try
+            {
+                // Kiểm tra ràng buộc cơ bản
+                //SelectedQuy.Validate();
+
+                // Kiểm tra quỹ đã tồn tại hay chưa
+                var existingQuy = Quys.FirstOrDefault(q => q.Name == SelectedQuy.Name && q.Type == SelectedQuy.Type && q.Id != SelectedQuy.Id);
+                if (existingQuy != null)
+                {
+                    throw new ArgumentException("Tên quỹ đã tồn tại.");
+                }
+
+                //if (SelectedQuy.Id == 0) // Thêm quỹ mới
+                //{
+                //    _quyRepository.AddQuy(SelectedQuy);
+                //    Quys.Add(SelectedQuy);
+                //}
+                else // Cập nhật quỹ
+                {
+                    //_quyRepository.UpdateQuy(SelectedQuy);
+                    var quyToUpdate = Quys.FirstOrDefault(q => q.Id == SelectedQuy.Id);
+                    if (quyToUpdate != null)
+                    {
+                        quyToUpdate.Name = SelectedQuy.Name;
+                        quyToUpdate.Type = SelectedQuy.Type;
+                        quyToUpdate.TotalTransaction = SelectedQuy.TotalTransaction;
+                        quyToUpdate.Status = SelectedQuy.Status;
+                    }
+                }
+
+                // Quay lại danh sách quỹ sau khi lưu
+                ShowQuyListView(null);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message, "Lỗi", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            }
+        }
+
+        private void LoadQuys()
+        {
+            try
+            {
+                Quys.Clear();
+                var quyList = _quyRepository.GetAllQuy();
+                foreach (var quy in quyList)
+                {
+                    Quys.Add(quy);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"Lỗi khi tải danh sách quỹ: {ex.Message}", "Lỗi",
+                    System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
